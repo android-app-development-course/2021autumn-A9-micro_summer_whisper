914d3ece70176437686879333070350c6258da81
pwh-pwh <plmoknpwh@163.com>
2021-11-28 19:11:52 +0800

    完成第一启动页搭建

---
+++ msw/app/src/main/java/org/coderpwh/micro_summer_whisper/activity/GuideActivity.kt
@@ -0,0 +1,11 @@
+package org.coderpwh.micro_summer_whisper.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide)
+    }
+}
