8719315193d3c9b421eb540632e9e8010273767a
pwh-pwh <plmoknpwh@163.com>
2021-12-01 15:14:46 +0800

    完成mainf的grid商品展示

--- msw/app/src/main/java/org/coderpwh/micro_summer_whisper/fragments/main/MainFragment.kt
+++ msw/app/src/main/java/org/coderpwh/micro_summer_whisper/fragments/main/MainFragment.kt
@@ -7,13 +7,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.GridView
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.qmuiteam.qmui.widget.QMUIViewPager
 import com.youth.banner.Banner
 import com.youth.banner.loader.ImageLoader
 import kotlinx.android.synthetic.main.fragment_main.*
 import org.coderpwh.micro_summer_whisper.R
+import org.coderpwh.micro_summer_whisper.models.CommodityTypeModel
+import org.w3c.dom.Text
 import java.util.*
+import java.util.zip.Inflater
 import kotlin.collections.ArrayList
 
 
@@ -24,7 +31,12 @@ import kotlin.collections.ArrayList
  */
 class MainFragment : Fragment() {
     var bannerList:ArrayList<String> = ArrayList()
-
+    lateinit var br:Banner
+    lateinit var gridView1:GridView
+    lateinit var viewPagerList:ArrayList<View>
+    var listData:ArrayList<CommodityTypeModel> = ArrayList()
+    val mPageSize:Int = 8
+    var totalPage:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,15 +49,47 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var inflate = inflater.inflate(R.layout.fragment_main, container, false)
-        initBanner(inflate)
+        initView(inflate)
+
+        
+        initBanner()
+        initGv1()
 
 
         return inflate
     }
 
+
+    fun initGv1(){
+        this.gridView1.adapter =  object :BaseAdapter(){
+            override fun getCount() = 8
+
+            override fun getItem(position: Int): Any {
+                return 1
+            }
+
+            override fun getItemId(position: Int): Long {
+                return position.toLong()
+            }
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                var fl = LayoutInflater.from(context).inflate(R.layout.item_gridview, null)
+                var tv = fl.findViewById<TextView>(R.id.proName)
+                var iv = fl.findViewById<ImageView>(R.id.imgUrl)
+                tv.setText("test")
+                iv.setImageResource(R.drawable.home53)
+                return fl
+            }
+        }
+
+    }
+
+    fun initView(view:View){
+        br = view.findViewById(R.id.banner)
+        gridView1 = view.findViewById(R.id.gridView1)
+    }
     //初始化广告页面
-    fun initBanner(view:View){
-        var br = view.findViewById<Banner>(R.id.banner)
+    fun initBanner(){
         bannerList.clear()
         bannerList.add("http://n.sinaimg.cn/translate/20170405/mcYi-fycxmks5744613.jpg")
         bannerList.add("http://n.sinaimg.cn/translate/20170405/5diM-fycwymx3911184.jpg")
@@ -59,5 +103,6 @@ class MainFragment : Fragment() {
         br.start()
 
     }
+
 
 }
